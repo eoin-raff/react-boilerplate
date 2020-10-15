6a1dadc97b84aaa3895f8b410d05abdc66b5e6e1
Eoin Rafferty <eora@tdc.dk>
2020-10-15 10:47:25 +0200

    added babel plugin for proposed JS syntax

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function App(props){
-    return <h1>Hello World.</h1>
+export default function App(props) {
+  const [count, setCount] = useState(0);
+  return (
+    <React.Fragment>
+      <h1>Hello World.</h1>
+      <h2>Count: {count}</h2>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+    </React.Fragment>
+  );
 }
