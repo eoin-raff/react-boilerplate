9f5aba03c4c1799aa5dc8a86fd32bbf20737b1e1
Eoin Rafferty <eora@tdc.dk>
2020-10-15 13:04:35 +0200

    added testing-library for react and jest-dom

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,12 @@
+import React from "react";
+import { cleanup, render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import App from "./App";
+
+afterEach(cleanup);
 describe("App", () => {
-  it("runs and passes", () => {
-    expect(true).toBe(true);
+  it("renders without error", () => {
+    render(<App />);
   });
 });
