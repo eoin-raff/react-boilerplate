c9817981e0b287b2962f1993c5b6ebeaeb6699a0
Eoin Rafferty <eora@tdc.dk>
2020-10-15 14:30:05 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const Warning = React.lazy(() => import('./Warning'));
-
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <React.Fragment>
       <h1>Hello World</h1>
-      <h2 className={count > 10 ? 'warning' : null}>Count: {count}</h2>
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
-      >
-        -
-      </button>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      {count > 10 && (
-        <React.Suspense fallback={null}>
-          <Warning />
-        </React.Suspense>
-      )}
     </React.Fragment>
   );
 }
+
 export default hot(module)(App);
--- src/Warning.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-export default function Warning() {
-  return <h1 className="warning">Warning!</h1>;
-}
